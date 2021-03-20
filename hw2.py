d70fc7884f7aecc862e9701f60fd7912b6900004
niklas <nheidemann@techfak.uni-bielefeld.de>
2021-03-20 16:58:00 +0100

    HW2 ready for handin

--- hw2.py
+++ hw2.py
@@ -16,45 +16,24 @@ class MyModel(tf.keras.Model):
     def __init__(self, output_units=2):
 
         super(MyModel, self).__init__()
-        self.layer0 = tf.keras.layers.Flatten(input_shape=(8,1))
-        self.layer = tf.keras.layers.Dense(output_units)
-        self.layer2 = tf.keras.layers.Dense(1)
+        self.layer2 = tf.keras.layers.Dense(32,activation=tf.keras.activations.tanh)
+        self.layer3 = tf.keras.layers.Dense(32,activation=tf.keras.activations.tanh)
+        self.layer4 = tf.keras.layers.Dense(output_units)
 
     def call(self, x_in):
 
         output = {}
-        x = self.layer0(x_in)
-        x = self.layer(x)
-        v = self.layer2(x)
+        #x = self.layer(x_in)
+        #v = self.layer2(x)
+        x=self.layer4(self.layer3(self.layer2(x_in)))
         output["q_values"] = x
         return output
 
 
-class ModelContunous(tf.keras.Model):
-    def __init__(self, output_units=2):
 
-        super(ModelContunous, self).__init__()
-
-        self.layer_mu = tf.keras.layers.Dense(output_units)
-        self.layer_sigma = tf.keras.layers.Dense(output_units, activation=None)
-        self.layer_v = tf.keras.layers.Dense(1)
-
-    def call(self, x_in):
-
-        sigmas = tf.exp(self.layer_sigma(x_in))
-        v = self.layer_v(x_in)
-        return v
-        '''
-        output = {}
-        mus = self.layer_mu(x_in)
-        output["mu"] = mus
-        output["sigma"] = sigmas
-
-        return output
-        '''
 
 if __name__ == "__main__":
-
+    #most of the stuff below as in gridworld/showcase
     kwargs = {
         "model": MyModel,
         "environment": "CartPole-v0",
@@ -73,11 +52,14 @@ if __name__ == "__main__":
 
     buffer_size = 5000
     test_steps = 1000
-    epochs = 20
+    epochs = 100
     sample_size = 1000
-    optim_batch_size = 8
+    optim_batch_size = 1
     saving_after = 5
 
+    learning_rate=0.001
+    gamma=0.85
+    
     # keys for replay buffer -> what you will need for optimization
     optim_keys = ["state", "action", "reward", "state_new", "not_done"]
 
@@ -95,11 +77,12 @@ if __name__ == "__main__":
 
     # get initial agent
     agent = manager.get_agent()
-    print(agent.model.trainable_variables)
 
-    model = MyModel(8)
-
+    optimizer=tf.keras.optimizers.Adam(learning_rate)
+    mse=tf.keras.losses.MeanSquaredError()
+    epsilon=1 #we'll multiply by 0.9 in each epoch
     for e in range(epochs):
+
         # training core
 
         # experience replay
@@ -114,45 +97,41 @@ if __name__ == "__main__":
         data_dict = dict_to_dict_of_datasets(sample_dict, batch_size=optim_batch_size)
 
         print("optimizing...")
-
-        # TODO: iterate through your datasets
-        model = MyModel()
-
-        for ac, st, r, sn, d in zip(data_dict["action"], data_dict["state"], data_dict["reward"], data_dict["state_new"], data_dict["not_done"]):
-            pred = model(st)
-
-            # TODO: optimize agent
-            model.optimize(
-                    optimizer = tf.keras.optimizers.Adam(0.001),
-                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
-            )
-
-            model.fit(
-                    dataset,
-                    epoch=1
-            )
-
-        dummy_losses = [
-            np.mean(np.random.normal(size=(64, 100)), axis=0) for _ in range(1000)
-        ]
-
-        new_weights = agent.model.get_weights()
-
-        # set new weights
-        manager.set_agent(new_weights)
-        # get new weights
-        agent = manager.get_agent()
+        itertuple = (data_dict["state"], data_dict["action"], data_dict["reward"], data_dict["state_new"], data_dict["not_done"])
+        lossSum=0
+        count=0
+        for state, action, reward, state_new, not_done in zip(*itertuple):
+            
+            with tf.GradientTape() as tape:
+                
+                if not_done:
+                    qtarget=tf.cast(reward,tf.float32)+gamma*tf.cast(agent.max_q(state_new),tf.float32)
+                else:
+                    qtarget=tf.cast(reward,tf.float32)
+                index=(state.numpy()[0,0],state.numpy()[0,1])
+                output=agent.q_val(state,action)
+                
+                loss=mse(qtarget,output)
+                gradients=tape.gradient(loss,agent.model.trainable_variables)
+                lossSum+=loss
+                count+=1
+                #print(gradients,loss)
+            optimizer.apply_gradients(zip(gradients,agent.model.trainable_variables))
+        manager.set_agent(agent.model.get_weights())
+           
         # update aggregator
+        loss=lossSum/count
+        loss=tf.reshape(loss,(1,))
         time_steps = manager.test(test_steps)
-        manager.update_aggregator(loss=dummy_losses, time_steps=time_steps)
+        manager.update_aggregator(loss=loss, time_steps=time_steps)
         # print progress
         print(
-            f"epoch ::: {e}  loss ::: {np.mean([np.mean(l) for l in dummy_losses])}   avg env steps ::: {np.mean(time_steps)}"
+            f"epoch ::: {e}  loss ::: {np.mean([np.mean(l) for l in loss])}   avg env steps ::: {np.mean(time_steps)}"
         )
 
         # yeu can also alter your managers parameters
-        manager.set_epsilon(epsilon=0.99)
+        epsilon*=0.9
+        manager.set_epsilon(epsilon)
 
         if e % saving_after == 0:
             # you can save models
